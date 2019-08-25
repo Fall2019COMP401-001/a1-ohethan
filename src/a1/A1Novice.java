0b42da6e07d9eac9f8c2a40d1170b0856262707f
ohethan <ohethan@live.unc.edu>
2019-08-25 14:06:55 -0400

    Implement A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,37 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Read in count of number of customers
+		int customerCount = scan.nextInt();
+		
+		// Read data for each customer
+		for (int i = 0; i < customerCount; i++) {
+			
+			// Read first and last name of customer
+			String firstName = scan.next();
+			String lastName = scan.next();
+			
+			// Initialize totalPrice to 0
+			double totalPrice = 0;
+			
+			// Read in count of items
+			int itemCount = scan.nextInt();
+			
+			// Read data for each item, add price to totalPrice
+			for (int j = 0; j < itemCount; j++) {
+				int quantity = scan.nextInt();
+				String item = scan.next();
+				double price = scan.nextDouble();
+				totalPrice += price * quantity;
+			}
+			
+			// format total price to two decimal places
+			String formattedTotal = String.format("%.2f", totalPrice);
+			
+			// Print output
+			System.out.println(firstName.substring(0,1) + ". " 
+					+ lastName + ": "
+					+ formattedTotal);
+		}
 	}
 }
